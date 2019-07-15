c8bf326d95d6237c6ec84102b115a7a380ebf70d
Rodney940 <rodney.joo@gmail.com>
2019-07-15 16:26:17 +0900

    Example Ver 0.9

--- example.py
+++ example.py
@@ -2,25 +2,36 @@ from tenseijingo import tenseijingo, ini
 from tenseijingo.TenseijingoHandler import handler
 import datetime
 import pdfkit
+import os
 
 
 if __name__ == '__main__':
-    download_path = r'test/test_html'
+    download_path = r'./html'
+    if not os.path.exists(download_path):
+        os.makedirs(download_path)
+
     user = ini.User()
+
     s = tenseijingo(user.id, user.password)
-    s.open()
+    try:
+        s.open()
 
-    article_list = s.get_list()
-    for article in article_list[1:10]:
-        content = s.get_content(article)
-        html = handler.making_html(content)
+        article_list = s.get_list()
+        for article in article_list[0:1]:
+            content = s.get_content(article)
+            content_date = content['datetime'].strftime("%Y%m%d")
 
-        fname = download_path + '/' + content['datetime'].strftime("%Y%m%d")
-
-        with open(fname + '.html', 'w') as f:
-            f.write(html)
-            config = pdfkit.configuration(wkhtmltopdf=r"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe")
-            # Todo : fix below phrase
-            pdfkit.from_file(f, fname + '.pdf')
-            # Todo : seperate making html (with session) and making pdf
-            f.close()
+            html_name = download_path + '/' + content_date + '.html'
+            if not os.path.exists(html_name):
+                print('Downloading.. ' + html_name.split('/')[-1])
+                html = handler.making_html(content)
+                with open(html_name, 'w') as f:
+                    f.write(html)
+                    #config = pdfkit.configuration(wkhtmltopdf=r"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe")
+                    # Todo : fix below phrase
+                    #pdfkit.from_file(f, fname + '.pdf')
+                    # Todo : seperate making html (with session) and making pdf
+                    f.close()
+    except ConnectionError as e:
+        print(e)
+    s.close()
