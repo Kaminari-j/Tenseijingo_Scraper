a7a659bc8733ddff28dae3d964a18d575d56bbb0
Joo Suhong <rodney.joo@gmail.com>
2019-12-16 21:15:04 +0900

    Update tenseijingo.py
    불필요한 content 습득을 제거

--- tenseijingo.py
+++ tenseijingo.py
@@ -3,7 +3,6 @@ from modules import TenseijingoModule
 import userinfo
 
 
-
 def get_html_with_date(date_from: str, date_to: str):
     # Todo : Convert this method to module and make available query by date from to
     download_path = r'./html'
@@ -31,22 +30,24 @@ def get_html_with_date(date_from: str, date_to: str):
         #       ex) dateFrom = '20191001', dateTo = '20191010'
         #           dt_list = ['20191005', '20191006', '20191007']
 
-        for upload_date in dt_list[idx_from:idx_to]:
-            print(upload_date)
-            content_dic = article_list[upload_date]
-            content = s.convert_content_bs_to_dict(content_dic['url'])
-            content_date = content['datetime'].strftime("%Y%m%d")
-
+        for content_date in dt_list[idx_from:idx_to]:
+            print(content_date, end=': ')
             html_name = download_path + '/' + content_date + '.html'
             if not os.path.exists(html_name):
+                content_dic = article_list[content_date]
+                content = s.convert_content_bs_to_dict(content_dic['url'])
+
                 print('Downloading.. ' + html_name.split('/')[-1])
                 html = TenseijingoModule.making_html(content)
                 with open(html_name, 'w') as f:
                     f.write(html)
                     f.close()
+            else:
+                print('skip')
     except ConnectionError as e:
         print(e)
 
 
 if __name__ == '__main__':
-    get_html_with_date('20191001', '20191003')
+    from datetime import date
+    get_html_with_date('20191001', date.today().strftime('%Y%m%d'))
